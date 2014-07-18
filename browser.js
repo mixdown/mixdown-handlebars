78464d7fc2d2938c36842aa099af2f1db14a6078
Tommy Messbauer <totally.bro@gmail.com>
2014-07-18 00:27:55 -0500

    added browser support for precompiled tempaltes

--- browser.js
+++ browser.js
@@ -1,15 +1,11 @@
 var HB = require('./lib/hb.js');
 var BrowserProvider = require('./lib/browser_provider.js');
 var _ = require('lodash');
-var handlebars = require('handlebars/runtime.js');
-
+var handlebars = require('handlebars/runtime.js')['default'];
 module.exports = HB.extend({
   init: function(options) {
     this._super(options);
-
-    // pass dev flag down.
-    view_options.handlebars = this.handlebars;
-    this.view_provider = new BrowserProvider();
+    this.view_provider = new BrowserProvider(options);
   },
   // create an instance of handlebars for this plugin to use.
   handlebars: handlebars,
